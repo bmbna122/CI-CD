3b45a17124a8d379cc24cb5e4a4bd2eced58e9b1
bmbna122 <bmbna122@gmail.com>
2026-09-10 05:57:20 +0000

    fixng health check to use container name

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('/health', (req, res) => {
 	res.status(200).json({ status: 'ok' });
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-	console.log('server running on port ${PORT}');
+app.listen(PORT,  () => {
+	console.log('Server running on port ${PORT}');
 });
